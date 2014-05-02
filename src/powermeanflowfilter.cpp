7be3e049cfafa7188358ff2a585a086a1a0577bd
Kacper Pluta <kacper.pluta@bdslabs.com.br>
2014-05-02 19:03:23 +0200

    Fix for nan values

--- src/powermeanflowfilter.cpp
+++ src/powermeanflowfilter.cpp
@@ -23,14 +23,20 @@ PowerMeanFlowFilter::PowerMeanFlowFilter( float power ) : MeanFlowFilter()
 
 Vector<float> PowerMeanFlowFilter::calculate_vector( unsigned int point )
 {
-  Vector<float> vector = MeanFlowFilter::calculate_vector ( point );
+  Vector < float > vector = MeanFlowFilter::calculate_vector ( point );
   vector *= std::pow ( vector.length(), POWER - 1 );
-  return vector;
+  if ( !std::isnan ( vector.length() ) )
+    return vector;
+  else
+    return Vector < float >();
 }
 
 Vector < float > PowerMeanFlowFilter::calculate_boundary_vector ( unsigned int point )
 {
-  Vector<float> vector = MeanFlowFilter::calculate_boundary_vector ( point );
+  Vector < float > vector = MeanFlowFilter::calculate_boundary_vector ( point );
   vector *= std::pow ( vector.length(), POWER - 1 );
-  return vector;
+  if ( !std::isnan ( vector.length() ) )
+    return vector;
+  else
+    return Vector < float >();
 }
